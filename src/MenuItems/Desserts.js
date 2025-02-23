15dc8dfd75c3a815ac356df5c4cad84b992dd5be
GabeKouki <gabekouki02@gmail.com>
2025-02-23 13:44:21 -0500

    a lot of progress on the menu, did a really cool calculation for the animation using inline styles

---
+++ src/MenuItems/Desserts.js
@@ -0,0 +1,9 @@
+const Desserts = [
+  { name: "CANNOLI DIP", price: 9.95 },
+  { name: "JUNIOR’S CHEESECAKE", price: 11.95 },
+  { name: "TIRAMISU", price: 10.95 },
+  { name: "HOT LAVA CAKE WITH ICE CREAM", price: 10.95 },
+  { name: "ASSORTED TARTUFOS", price: 10.95 },
+];
+
+export default Desserts;
